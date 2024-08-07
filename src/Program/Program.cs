f4e7d8c3155b578389ed401efa4de8081471b645
AgustinPalladino <agustin.palladino@correo.ucu.edu.uy>
2024-08-07 20:41:30 -0300

    Implementación de la operación feature-multiplication

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -1,4 +1,8 @@
-﻿public class Multiplication
+﻿Console.WriteLine(Suma.Sumar(1, 2));
+Console.WriteLine(Resta.Restar(3, 4));
+Console.WriteLine(Multiplicacion.Multiplicar(5, 6));
+Console.WriteLine(Division.Dividir(7, 8));
+public class Multiplication
 {
     public static int Multiply(int a, int b)
     {
