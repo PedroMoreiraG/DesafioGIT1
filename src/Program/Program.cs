38acc8126e8180427c9b10993652b9db002daaa6
AgustinPalladino <agustin.palladino@correo.ucu.edu.uy>
2024-08-12 19:32:54 -0300

    Ultimos cambios

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -1,7 +1,26 @@
-﻿Console.WriteLine(Suma.Sumar(1, 2));
-Console.WriteLine(Resta.Restar(3, 4));
-Console.WriteLine(Multiplicacion.Multiplicar(5, 6));
-Console.WriteLine(Division.Dividir(7, 8));
+Console.WriteLine(Addition.Add(7, 2));
+Console.WriteLine(Substraction.Substract(10, 1));
+Console.WriteLine(Multiplication.Multiply(3, 3));
+Console.WriteLine(Division.Divide(18, 2));
+
+// Esta clase implementa la operación Suma
+public class Addition
+{
+    public static int Add(int a, int b)
+    {
+        return a + b;
+    }
+}
+
+// Esta clase implementa la operación Resta
+public class Substraction
+{
+    public static int Substract(int a, int b)
+    {
+        return a - b;
+    }
+}
+// Esta clase implementa la operación Multiplicación
 public class Multiplication
 {
     public static int Multiply(int a, int b)
@@ -9,10 +28,16 @@ public class Multiplication
         return a * b;
     }
 }
-public class Power
+
+
+// Esta clase Implementa la operacion Division
+
+public class Division
 {
-    public static int Squared(int a)
+    public static double Divide(int a, int b)
     {
-        return a * a;
+        return (double)a / b;
     }
 }
+
+// Ultima actualización
