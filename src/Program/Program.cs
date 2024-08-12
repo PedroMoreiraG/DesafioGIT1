8e190228a13578fa607610c48688b8b53d932d03
Esteban <esteban.duran@correo.ucu.edu.uy>
2024-08-12 19:06:50 -0300

    Revertir cambio incorrecto, resultado resta 0

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -8,6 +8,6 @@ public class Substraction
 {
     public static int Substract(int a, int b)
     {
-        return 0;   //Cambio incorrecto, retorna 0
+        return 0; //Cambio incorrecto
     }
 }
